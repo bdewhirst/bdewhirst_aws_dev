c9c0efca2e0947bc5fb46bc67549b3ed9eb58039
bdewhirst <b.dewhirst@gmail.com>
2022-08-12 10:07:37 -0400

    remove PyCharm defaults from example script

---
+++ main.py
@@ -0,0 +1,10 @@
+NAME: str = "Brian Dewhirst"
+
+
+def hello_world() -> None:
+    """Print the constant 'NAME'"""
+    print(NAME)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == "__main__":
+    hello_world()
